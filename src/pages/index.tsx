1bf4be2a6045cc29a4a6be2a468c05afc8f407d6
Lance <laclance@gmail.com>
2022-04-24 12:24:21 +0200

    Add page redirection;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,15 @@
 import { Box } from '@chakra-ui/react';
-import { useSession } from 'next-auth/react';
+import { getSession, useSession } from 'next-auth/react';
 import React, { useState } from 'react';
 import ErrorBoundary from 'src/components/ErrorBoundary';
 import Jobs from 'src/components/Jobs';
 import Portfolio from 'src/components/Portfolio';
 import Settings from 'src/components/Settings';
 import styled from 'styled-components';
-import Welcome from 'src/components/Welcome';
 import StyledSideNav from 'src/styles/SideNav.style';
 import Orders from 'src/components/Orders';
 
-const MessageText = styled.h1`
-  color: #333;
-  font-weight: bold;
-`;
-
-const Container = styled.div`
-  background: lightGray;
-  align-content: center;
-  justify-items: center;
-  justify-content: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-width: 100vw;
-  min-height: 100vh;
-`;
-
-const DashBoardContainer = styled.div`
+const HomeContainer = styled.div`
   background: lightGray;
   display: flex;
   height: 100vh;
@@ -38,77 +20,50 @@ const DashBoardContainer = styled.div`
 export default function Index() {
   const { data: session } = useSession();
   const { user } = session || {};
+  const subscriptionActive = user.subscriptionUntil >= new Date();
   const [selectedPage, setSelectedPage] = useState('settings');
 
-  // return (
-  //   <ErrorBoundary>
-  //     <DashBoardContainer>
-  //       <StyledSideNav selectedPage={selectedPage} setSelectedPage={(page) => setSelectedPage(page)} />
+  function switchPage(page: string) {
+    if (user.binance?.apiKey && user.binance?.apiSecret && !subscriptionActive) {
+      setSelectedPage(page);
+    } else {
+      let message = '';
 
-  //       {selectedPage === 'portfolio' && <Portfolio />}
+      if (!user.binance?.apiKey || !user.binance?.apiSecret) message = 'Binance api keys required. ';
 
-  //       {selectedPage === 'jobs' && <Jobs defaultTimezone={user?.timezone || ''} />}
+      if (!subscriptionActive) message += 'Subscription required.';
 
-  //       {selectedPage === 'settings' && (
-  //         <Settings
-  //           user={{
-  //             id: '',
-  //             email: 'laclance@gmail.com',
-  //             timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-  //             telegram: {
-  //               botToken: '',
-  //               chatId: '',
-  //               enabled: false,
-  //             },
-  //             binance: {
-  //               update: false,
-  //             },
-  //           }}
-  //         />
-  //       )}
-  //     </DashBoardContainer>
-  //   </ErrorBoundary>
-  // );
-  // new Date(new Date().setFullYear(new Date().getFullYear() + 1))
-
-  if (!user) return <Welcome />;
-
-  const subscriptionActive = user.subscriptionUntil >= new Date();
-
-  if (user.binance?.apiKey && user.binance?.apiSecret && !subscriptionActive) {
-    return (
-      // @ts-ignore
-      <ErrorBoundary>
-        <DashBoardContainer>
-          <StyledSideNav selectedPage={selectedPage} setSelectedPage={(page) => setSelectedPage(page)} />
-          {selectedPage === 'portfolio' && <Portfolio />}
-          {selectedPage === 'settings' && <Settings user={user} />}
-          {selectedPage === 'jobs' && <Jobs defaultTimezone={user?.timezone || ''} />}
-          {selectedPage === 'orders' && (
-            <Orders defaultTimezone={user?.timezone || ''} navigateToJobs={() => setSelectedPage('jobs')} />
-          )}
-        </DashBoardContainer>
-      </ErrorBoundary>
-    );
+      alert(message);
+    }
   }
 
   return (
     // @ts-ignore
     <ErrorBoundary>
-      <Container>
-        <Box>
-          {(!user.binance?.apiKey || !user.binance?.apiSecret) && (
-            <Box>
-              <MessageText>Binance api keys required</MessageText>
-            </Box>
-          )}
-          {!subscriptionActive && (
-            <Box>
-              <MessageText>Subscription required</MessageText>
-            </Box>
-          )}
-        </Box>
-      </Container>
+      <HomeContainer>
+        <StyledSideNav selectedPage={selectedPage} setSelectedPage={switchPage} />
+        {selectedPage === 'portfolio' && <Portfolio />}
+        {selectedPage === 'settings' && <Settings user={user} />}
+        {selectedPage === 'jobs' && <Jobs defaultTimezone={user?.timezone || ''} />}
+        {selectedPage === 'orders' && (
+          <Orders defaultTimezone={user?.timezone || ''} navigateToJobs={() => setSelectedPage('jobs')} />
+        )}
+      </HomeContainer>
     </ErrorBoundary>
   );
+}
+
+export async function getServerSideProps({ req }: any) {
+  const session = await getSession({ req });
+
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/welcome',
+      },
+    };
+  }
+
+  return { props: {} };
 }
