badece81e449d05304a300f40bf8672f320cf0b7
wesatat <wesatat@gmail.com>
2022-05-10 14:41:59 +0200

    Added more styling to the TRade settings modal

--- src/components/TradeSettingsModal.tsx
+++ src/components/TradeSettingsModal.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode, CSSProperties, useState } from 'react';
 import styled from 'styled-components';
 import {
+  Alert,
+  AlertIcon,
   Button,
   flexbox,
   FormControl,
@@ -30,7 +32,9 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   border: 2px solid grey;
-  height: 700px;
+  padding: 10px;
+  height: 90vh;
+  overflow: scroll;
   width: 500px;
   & h1 {
     text-align: center;
@@ -43,6 +47,20 @@ const Container = styled.div`
   .labelWrapper {
     display: flex;
     padding: 15px;
+    align-items: center;
+    justify-content: space-between;
+    border: 1px solid grey;
+    border-radius: 5px;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.19);
+    margin: 5px;
+  }
+  .inputField {
+    width: 150px;
+    align-items: center;
+  }
+
+  .inputButton {
+    align-items: center;
   }
   .footer {
     text-align: center;
@@ -55,14 +73,21 @@ function TradeSettingsModal({ className }: Props) {
 
   const parse = (val) => val.replace(/^\$/, '');
 
-  const [value, setValue] = React.useState('7');
+  const [margin, setMargin] = React.useState('7');
+  const [wholePoisitonRatio, setWholePoisitonRatio] = React.useState('7');
+  const [wholePoisitonCallback, setWholePoisitonCallback] = React.useState('7');
+  const [buyIn, setBuyIn] = React.useState('7');
+  const [subPos, setSubPos] = React.useState('7');
 
   return (
     <Container>
       <span>
         <h1>Trade Settings</h1> <Button variant="link">Confirm</Button>
       </span>
-
+      <Alert fontSize="xs" status="warning" textAlign="left">
+        <AlertIcon />
+        The first buy in amount is calculated accoridng to the currency pair,principle and trade unit
+      </Alert>
       <FormControl as="fieldset" alignItems="center">
         <div className="labelWrapper">
           <FormLabel htmlFor="positionDoubled" mb="0">
@@ -74,13 +99,16 @@ function TradeSettingsModal({ className }: Props) {
           <FormLabel htmlFor="marginCallLimit" mb="0">
             Margin Call Limit
           </FormLabel>
+
           <NumberInput
             id="marginCallLimit"
-            onChange={(valueString) => setValue(parse(valueString))}
-            value={formatTime(value)}
+            onChange={(valueString) => setMargin(parse(valueString))}
+            value={formatTime(margin)}
             min={0}
           >
-            <NumberInputField />
+            <div className="inputField">
+              <NumberInputField />
+            </div>
             <NumberInputStepper>
               <NumberIncrementStepper />
               <NumberDecrementStepper />
@@ -93,15 +121,14 @@ function TradeSettingsModal({ className }: Props) {
           </FormLabel>
           <NumberInput
             id="wholePositionRatio"
-            onChange={(valueString) => setValue(parse(valueString))}
-            value={formatPercent(value)}
+            onChange={(valueString) => setWholePoisitonRatio(parse(valueString))}
+            value={formatPercent(wholePoisitonRatio)}
             min={0}
           >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
+            <div className="inputField">
+              <NumberInputField />
+            </div>
+            <NumberInputStepper></NumberInputStepper>
           </NumberInput>
         </div>
         <div className="labelWrapper">
@@ -110,60 +137,68 @@ function TradeSettingsModal({ className }: Props) {
           </FormLabel>
           <NumberInput
             id="wholePositionCallBack"
-            onChange={(valueString) => setValue(parse(valueString))}
-            value={formatPercent(value)}
+            onChange={(valueString) => setWholePoisitonCallback(parse(valueString))}
+            value={formatPercent(wholePoisitonCallback)}
             min={0}
           >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
+            <div className="inputField">
+              <NumberInputField />
+            </div>
+            <NumberInputStepper></NumberInputStepper>
           </NumberInput>
         </div>
         <div className="labelWrapper">
           <FormLabel htmlFor="wholePositionRatio" mb="0">
-            Margin configuration <Button rightIcon={<ArrowForwardIcon />} colorScheme="grey" variant="link"></Button>
+            Margin configuration
           </FormLabel>
+          <div className="inputButton">
+            {' '}
+            <Button rightIcon={<ArrowForwardIcon />} colorScheme="grey" variant="link"></Button>
+          </div>
         </div>
         <div className="labelWrapper">
-          <FormLabel htmlFor="Buy in Callback" mb="0">
-            Buy in callback
-          </FormLabel>
+          <div className="inputFieldLabel">
+            <FormLabel htmlFor="Buy in Callback" mb="0">
+              Buy in callback
+            </FormLabel>
+          </div>
           <NumberInput
             id="Buy in Callback"
-            onChange={(valueString) => setValue(parse(valueString))}
-            value={formatPercent(value)}
+            onChange={(valueString) => setBuyIn(parse(valueString))}
+            value={formatPercent(buyIn)}
             min={0}
           >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
+            <div className="inputField">
+              <NumberInputField />
+            </div>
+            <NumberInputStepper></NumberInputStepper>
           </NumberInput>
         </div>
         <div className="labelWrapper">
           <FormLabel htmlFor="distributedProfit" mb="0">
             Distributed & Take Profit Allocation{' '}
-            <Button rightIcon={<ArrowForwardIcon />} colorScheme="grey" variant="link"></Button>
           </FormLabel>
+          <div className="inputButton">
+            {' '}
+            <Button rightIcon={<ArrowForwardIcon />} colorScheme="grey" variant="link"></Button>
+          </div>
         </div>
         <div className="labelWrapper">
-          <FormLabel htmlFor="subposTakeProfitCallback" mb="0">
-            Sub-position take-profit callback
-          </FormLabel>
+          <div className="inputFieldLabel">
+            <FormLabel htmlFor="subposTakeProfitCallback" mb="0">
+              Sub-position take-profit callback
+            </FormLabel>
+          </div>
           <NumberInput
             id="subposTakeProfitCallback"
-            onChange={(valueString) => setValue(parse(valueString))}
-            value={formatPercent(value)}
+            onChange={(valueString) => setSubPos(parse(valueString))}
+            value={formatPercent(subPos)}
             min={0}
           >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
+            <div className="inputField">
+              <NumberInputField />
+            </div>
+            <NumberInputStepper></NumberInputStepper>
           </NumberInput>
         </div>
 
